24a0b0385b25243354fb9c350e963f9a97942f18
lukasz0707 <lukaszs0707@gmail.com>
2022-10-04 00:34:48 +0200

    ep4

--- handlers/products.go
+++ handlers/products.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/lukasz0707/go-microservices/data"
 )
@@ -21,6 +24,27 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		p.GetProducts(rw, r)
 		return
 	}
+	if r.Method == http.MethodPost {
+		p.AddProduct(rw, r)
+		return
+	}
+	if r.Method == http.MethodPut {
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idstring := g[0][1]
+		id, err := strconv.Atoi(idstring)
+		if err != nil {
+			fmt.Println(err)
+		}
+		p.UpdateProducts(id, rw, r)
+		return
+	}
 
 	// catch all
 	rw.WriteHeader(http.StatusMethodNotAllowed)
@@ -31,5 +55,43 @@ func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "unable to marshal json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) AddProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	data.AddProduct(prod)
+	p.l.Printf("Prof: %#v", prod)
+}
+
+func (p *Products) UpdateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Product")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		fmt.Println(err)
+		http.Error(rw, "product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		fmt.Println(err)
+		http.Error(rw, "product not found", http.StatusInternalServerError)
+		return
 	}
 }
